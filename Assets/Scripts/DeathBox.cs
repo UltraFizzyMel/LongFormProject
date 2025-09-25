433e90c85604f212aa76af85304f6557803e4cea
RhettiBOi <vdhrhett@icloud.com>
2025-09-25 22:22:40 +0200

    CrossHair/ started playtesting
    tried to add a deathbox but will give it a better look in the morning

---
+++ Assets/Scripts/DeathBox.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    public GameObject RespawnPoint;
+
+    public GameObject PLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Deathbox"))
+        {
+            Debug.Log("Dead");
+            PLayer.transform.position = RespawnPoint.transform.position;
+        }
+    }
+}
