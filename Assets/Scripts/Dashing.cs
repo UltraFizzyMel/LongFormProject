ced218e6877696663b6bbc81349781f609f8c1a4
UltraFizzyMel <2344445@students.wits.ac.za>
2025-10-01 13:22:35 +0200

    Better fix

--- Assets/Scripts/Dashing.cs
+++ Assets/Scripts/Dashing.cs
@@ -1,4 +1,4 @@
-using UnityEditor.ShaderGraph;
+//using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class Dashing : MonoBehaviour
--- Assets/Scripts/FInish.cs
+++ Assets/Scripts/FInish.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     public GameObject WS;
     public PortalGun PG;
     public FirstPersonControls FPC;
+
     //public Portal PC;
     // Start is called before the first frame update
     void Start()
@@ -26,9 +28,10 @@ public class Finish : MonoBehaviour
         {
             Debug.Log("You Win!");
             FPC.canPlayerMove = false;
-            WS.SetActive(true);
+            //WS.SetActive(true);
             //PG.enabled = false;
             FPC.enabled = false;
+            SceneManager.LoadScene("Menu");
             //PC.enabled = false;
         }
     }
